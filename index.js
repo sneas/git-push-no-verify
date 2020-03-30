2d1b5ec89cce4e1392fedccec1772a50e3652ecb
Dima Snisarenko <snisarenkodima@gmail.com>
2020-03-30 10:53:33 +0200

    fix: properly output stdout and stderr

--- index.js
+++ index.js
@@ -6,33 +6,25 @@ const pushUpstream = (prevError, prevStderr) => {
     const gitPush = prevStderr.match(/git push --set-upstream.+/gim).pop();
 
     if (!gitPush) {
-        console.error(prevStderr);
+        process.stderr.write(prevStderr);
         process.exit(prevError.code);
         return;
     }
 
     exec(`${gitPush} --no-verify`, (error, stdout, stderr) => {
-        if (!error) {
-            console.log(stdout);
-            return;
-        }
-
-        console.error(stderr);
-        process.exit(error.code);
+        process.stdout.write(stdout);
+        process.stderr.write(stderr);
+        process.exit(error ? error.code : 0);
     });
 };
 
 exec('git push --no-verify', (error, stdout, stderr) => {
-    if (!error) {
-        console.log(stdout);
-        return;
-    }
-
-    if (error.code === 128) {
+    if (error && error.code === 128) {
         pushUpstream(error, stderr);
         return;
     }
 
-    console.error(stderr);
-    process.exit(stderr.code);
+    process.stdout.write(stdout);
+    process.stderr.write(stderr);
+    process.exit(error ? error.code : 0);
 });
