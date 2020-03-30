51d3c7ee3849853c149aba617a26e6cdf3ffffc0
Dima Snisarenko <snisarenkodima@gmail.com>
2020-03-30 10:37:32 +0200

    feat: add gpn command

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 #!/usr/bin/env node
 
-console.log('TBD');
+const { exec } = require("child_process");
+
+const pushUpstream = (prevError, prevStderr) => {
+    const gitPush = prevStderr.match(/git push --set-upstream.+/gim).pop();
+
+    if (!gitPush) {
+        console.error(prevStderr);
+        process.exit(prevError.code);
+        return;
+    }
+
+    exec(`${gitPush} --no-verify`, (error, stdout, stderr) => {
+        if (!error) {
+            console.log(stdout);
+            return;
+        }
+
+        console.error(stderr);
+        process.exit(error.code);
+    });
+};
+
+exec('git push --no-verify', (error, stdout, stderr) => {
+    if (!error) {
+        console.log(stdout);
+        return;
+    }
+
+    if (error.code === 128) {
+        pushUpstream(error, stderr);
+        return;
+    }
+
+    console.error(stderr);
+    process.exit(stderr.code);
+});
